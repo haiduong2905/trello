1c2748c4227353b56965e1d239b1a9fe912e832b
Duong <haiduong2905@gmail.com>
2022-02-13 22:26:15 +0700

    dnd

--- src/components/AppBar/AppBar.js
+++ src/components/AppBar/AppBar.js
@@ -2,9 +2,9 @@ import React from 'react'
 import './AppBar.scss'
 
 function AppBar() {
-	return (
-		<nav className="navbar-app"></nav>
-	)
+    return (
+        <nav className="navbar-app">Navbar</nav>
+    )
 }
 
 export default AppBar
--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,13 +1,12 @@
-import React from 'react'
 import './Card.scss'
 
 function Card(props) {
     const { card } = props
     return (
-        <li className="task-item">            
-            { card.cover && <img className="card-cover" src={card.cover} alt='123' /> }
-            { card.title }
-        </li>
+        <div className="task-item">
+            {card.cover && <img className="card-cover" src={card.cover} alt='123' />}
+            {card.title}
+        </div>
     )
 }
 
